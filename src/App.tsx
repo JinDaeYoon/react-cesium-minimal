02b012c493ef8438bc991bae889d4c40d6d6dd22
Hyeokjoo Mun <hyeokjoo@angelswing.io>
2019-11-21 12:19:36 +0900

    ASdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cesium from "cesium";
 
 const App: React.FC = () => {
+  const viewer = new Cesium.Viewer(document.getElementById("cesium")!);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="cesium">
     </div>
   );
 }
